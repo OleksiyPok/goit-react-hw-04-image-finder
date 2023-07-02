4cdfba614fde7d6e062f4dd150a8d2c17292af51
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-07-02 11:06:37 +0200

    doRequest has been moved into useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,8 +18,45 @@ const App = () => {
   const [gallery, setGallery] = useState([]);
 
   useEffect(() => {
+    const doRequest = async () => {
+      if (!searchQuery) {
+        return;
+      }
+
+      if (currentPage === totalPages) {
+        toast.info(`This is the last page`);
+      }
+
+      setIsLoading(true);
+
+      try {
+        const responseData = await Api.getData(searchQuery, currentPage);
+        const newGallery = responseData.hits;
+
+        setGallery([...gallery, ...newGallery]);
+
+        if (currentPage === 1) {
+          const imagesPerPage = newGallery.length;
+          const totalImages = responseData.total;
+          const totalPages = Math.ceil(totalImages / imagesPerPage);
+
+          if (newGallery.length === 0) {
+            toast.error(`No images found for your request!`);
+          } else {
+            toast.success(`Found ${totalImages} images matching your request`);
+          }
+
+          setTotalPages(totalPages);
+        }
+      } catch (error) {
+        return console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     doRequest();
-  }, [searchQuery, currentPage]);
+  }, [searchQuery, currentPage, gallery, totalPages]);
 
   const handleOnSearch = currentSearchQuery => {
     if (currentSearchQuery !== searchQuery) {
@@ -35,43 +72,6 @@ const App = () => {
   const handleLoadMore = () => {
     if (currentPage < totalPages) {
       setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const doRequest = async () => {
-    if (!searchQuery) {
-      return;
-    }
-
-    if (currentPage === totalPages) {
-      toast.info(`This is the last page`);
-    }
-
-    setIsLoading(true);
-
-    try {
-      const responseData = await Api.getData(searchQuery, currentPage);
-      const newGallery = responseData.hits;
-
-      setGallery([...gallery, ...newGallery]);
-
-      if (currentPage === 1) {
-        const imagesPerPage = newGallery.length;
-        const totalImages = responseData.total;
-        const totalPages = Math.ceil(totalImages / imagesPerPage);
-
-        if (newGallery.length === 0) {
-          toast.error(`No images found for your request!`);
-        } else {
-          toast.success(`Found ${totalImages} images matching your request`);
-        }
-
-        setTotalPages(totalPages);
-      }
-    } catch (error) {
-      return console.log(error.message);
-    } finally {
-      setIsLoading(false);
     }
   };
 
